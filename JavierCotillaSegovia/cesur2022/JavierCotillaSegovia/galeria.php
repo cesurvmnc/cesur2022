0c9e7d55a80080c5b1cbe6a8f8766378d864e403
javicoti10 <117991004+javicoti10@users.noreply.github.com>
2022-12-19 10:15:49 +0100

    Add files via upload

---
+++ JavierCotillaSegovia/cesur2022/JavierCotillaSegovia/galeria.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="Web sencilla de im&aacute;genes">
+	<meta name="keywords" content="HTML5, CSS3, JavaScript">
+  <link rel="stylesheet" href="style.css">
+	<title>Flores</title>
+
+	<style>
+		div{
+			background-color: greenyellow;
+			padding: 10px;
+		}
+	</style>
+
+
+</head>
+<body>
+
+
+  <h1 class="titulo">EJEMPLO EJERCICIO. Cambios en Examen Javier Cotilla Segovia</h1>
+  <h2 class="span">Una selecci&oacute;n de flores.</h2>
+
+  
+
+   <div class="centro">
+ 
+      <img src="imagenes/1.jpg" alt="Flores de corte" width="100" height="100">
+    
+      <span class="span">Foto 1: Flores de corte</span>   
+     
+    
+      <img src="imagenes/2.jpg" alt="Mirabilis Jalapa" width="100" height="100">
+     
+      <span class="span">Foto 2: Flor de Mirabilis Jalapa</span>
+     
+    
+      <img src="imagenes/3.jpg" alt="Strelitzia" width="100" height="100">
+     
+      <span class="span">Foto3: Strelitzia. Flores de las aves.</span>
+    
+    
+       <img src="imagenes/4.jpg" alt="Flor del opio" width="100" height="100">
+      
+       <span class="span">Foto 4: Flor del opio.</span>
+      
+   
+       <img src="imagenes/5.jpg" alt="Rosas" width="100" height="100">
+     
+       <span class="span">Foto 5: Rosas</span>
+     
+    
+      <img src="imagenes/6.jpg" alt="Pensamiento" width="100" height="100">
+     
+      <span class="span">Foto 6: Pensamiento</span>
+  </div>
+     
+
+  <p class="span">Copyright 2022, www.cesurformaion.com</p>
+
+
+  <h1 style="color:red">
+  <?php
+  echo 'Autor:Javier Cotilla Segovia'
+  ?>
+  </h1>
+</body>
+</html>
