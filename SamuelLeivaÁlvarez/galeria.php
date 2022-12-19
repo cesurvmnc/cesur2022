d11941920c80e18205f6a6e9c42f6c78fada4de1
Samuel <samu_5@hotmail.es>
2022-12-19 09:51:12 +0100

    Examen samu terminado

---
+++ SamuelLeivaÁlvarez/galeria.php
@@ -0,0 +1,90 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="Web sencilla de im&aacute;genes">
+	<meta name="keywords" content="HTML5, CSS3, JavaScript">
+	<title>Flores</title>
+
+	<style>
+		div{
+			background-color: greenyellow;
+			padding: 10px;
+		}
+
+    h1.mod1{
+      color:blue;
+      background-color:pink;
+    }
+    p{
+      color:orange;
+      background-color:cyan;
+    }
+	</style>
+
+
+</head>
+<body>
+
+
+  <h1>EJEMPLO EJERCICIO. Galer&iacute;as de im&aacute;genes</h1>
+  <h2>Una selecci&oacute;n de flores.</h2>
+
+  <h1 class="mod1">Esta es mi primera modificacion profe, la segunda sera el estilo de este h1 incluido en la etiqueta style que esta en el head </h1>
+
+  <p>Esta es la ultima modificacion, un saludo profe</p>
+
+
+  
+
+   
+ 
+      <img src="imagenes/1.jpg" alt="Flores de corte" width="200" height="200">
+    
+      Foto 1: Flores de corte   
+     
+    
+      <img src="imagenes/2.jpg" alt="Mirabilis Jalapa" width="200" height="200">
+     
+      Foto 2: Flor de Mirabilis Jalapa
+     
+    
+      <img src="imagenes/3.jpg" alt="Strelitzia" width="200" height="200">
+     
+     Foto3: Strelitzia. Flores de las aves.
+    
+    
+       <img src="imagenes/4.jpg" alt="Flor del opio" width="200" height="200">
+      
+      Foto 4: Flor del opio.
+      
+   
+       <img src="imagenes/5.jpg" alt="Rosas" width="200" height="200">
+     
+      Foto 5: Rosas
+     
+    
+      <img src="imagenes/6.jpg" alt="Pensamiento" width="200" height="200">
+     
+     Foto 6: Pensamiento
+    
+     
+
+  <p>Copyright 2022, www.cesurformaion.com</p>
+
+
+  <h1 style="color:red">
+  <?php
+
+      echo'Autor:(Samuel Leiva Álvarez)';
+
+      ?>
+
+
+
+</h1>
+
+
+  
+</body>
+</html>
