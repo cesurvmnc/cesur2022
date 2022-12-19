f69be0279780a05f219f21295d6435538ff982fc
Danirmd22 <117454588+Danirmd22@users.noreply.github.com>
2022-12-19 10:15:30 +0100

    Add files via upload

---
+++ DanielRamosMartinez/cesur2022/DanielRamosMartinez/galeria.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="Web sencilla de im&aacute;genes">
+	<meta name="keywords" content="HTML5, CSS3, JavaScript">
+	<title>Flores</title>
+  <link rel="stylesheet" href="estilos.css">
+
+	<style>
+		div{
+			background-color: greenyellow;
+			padding: 10px;
+		}
+	</style>
+
+
+</head>
+<body>
+<header>
+  <nav>
+    <ul>
+      <li><a href="#inicio">Inicio</a></li>
+      <li><a href="#contacto">Contacto</a></li>
+      <li><a href="#sobre-mi">Sobre mí</a></li>
+      <li><a href="#skills">Skills</a></li>
+    </ul>
+  </nav>
+</header>
+<h1>Hola esto es un cambio para el examen de Daniel Ramos</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic harum quo modi fugit eveniet id, sunt officiis quas est sequi quia perferendis. Suscipit dolor fuga autem, sint blanditiis perspiciatis neque?</p>
+
+  <h1>EJEMPLO EJERCICIO. Galer&iacute;as de im&aacute;genes</h1>
+  <h2>Una selecci&oacute;n de flores.</h2>
+
+  
+
+   
+ 
+      <img src="imagenes/1.jpg" alt="Flores de corte" width="200" height="200">
+    
+      Foto 1: Flores de corte   
+     
+    
+      <img src="imagenes/2.jpg" alt="Mirabilis Jalapa" width="200" height="200">
+     
+      Foto 2: Flor de Mirabilis Jalapa
+     
+    
+      <img src="imagenes/3.jpg" alt="Strelitzia" width="200" height="200">
+     
+     Foto3: Strelitzia. Flores de las aves.
+    
+    
+       <img src="imagenes/4.jpg" alt="Flor del opio" width="200" height="200">
+      
+      Foto 4: Flor del opio.
+      
+   
+       <img src="imagenes/5.jpg" alt="Rosas" width="200" height="200">
+     
+      Foto 5: Rosas
+     
+    
+      <img src="imagenes/6.jpg" alt="Pensamiento" width="200" height="200">
+     
+     Foto 6: Pensamiento
+    
+     
+
+  <p>Copyright 2022, www.cesurformaion.com</p>
+
+<h1 style="color:red">
+  <?php
+        echo 'Autor:(DANIEL RAMOS MARTINEZ)';
+        ?>
+  </H1>
+</body>
+</html>
