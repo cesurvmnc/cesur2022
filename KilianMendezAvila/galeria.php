5ddbd5a6480c262d67e20e2dc4afd0b33a0a64fe
Kiliancesur <117454852+Kiliancesur@users.noreply.github.com>
2022-12-19 09:49:56 +0100

    Subo mi carpeta

---
+++ KilianMendezAvila/galeria.php
@@ -0,0 +1,75 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="Web sencilla de im&aacute;genes">
+	<meta name="keywords" content="HTML5, CSS3, JavaScript">
+	<title>Flores</title>
+
+	<style>
+		div{
+			background-color: greenyellow;
+			padding: 10px;
+		}
+	</style>
+
+
+</head>
+<body>
+
+  <header>
+    <p>Esto es una cabezera con una palabra en color <span style="color:red">rojo</span></p>
+  </header>
+
+  <h1 style="color:blue;">Esto es un parrafo h1 para el examen<h1>
+
+  <p>Esto es otro parrafo para el <i style="color: orange;">examen</i></p>
+
+  <h1>EJEMPLO EJERCICIO. Galer&iacute;as de im&aacute;genes</h1>
+  <h2>Una selecci&oacute;n de flores.</h2>
+
+  
+
+   
+ 
+      <img src="imagenes/1.jpg" alt="Flores de corte" width="200" height="200">
+    
+      Foto 1: Flores de corte   
+     
+    
+      <img src="imagenes/2.jpg" alt="Mirabilis Jalapa" width="200" height="200">
+     
+      Foto 2: Flor de Mirabilis Jalapa
+     
+    
+      <img src="imagenes/3.jpg" alt="Strelitzia" width="200" height="200">
+     
+     Foto3: Strelitzia. Flores de las aves.
+    
+    
+       <img src="imagenes/4.jpg" alt="Flor del opio" width="200" height="200">
+      
+      Foto 4: Flor del opio.
+      
+   
+       <img src="imagenes/5.jpg" alt="Rosas" width="200" height="200">
+     
+      Foto 5: Rosas
+     
+    
+      <img src="imagenes/6.jpg" alt="Pensamiento" width="200" height="200">
+     
+     Foto 6: Pensamiento
+    
+     
+
+  <p>Copyright 2022, www.cesurformaion.com</p>
+
+  <h1 style="color: red">
+    <?php
+      echo 'Autor: Kilian Mendez Avila';
+    ?>
+  </h1>
+  
+</body>
+</html>
