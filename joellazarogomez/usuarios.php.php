7244dfbfbe5bf8e542b543b7aaadd37b2b15b009
Joel Lázaro Gómez <joel.s299531@cesurformacion.com>
2022-12-19 09:48:30 +0100

    examenjoel

---
+++ joellazarogomez/usuarios.php.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.0
+ */
+
+/**
+ * Database `bbddcesur`
+ */
+
+/* `bbddcesur`.`usuarios` */
+$usuarios = array(
+  array('id' => '100','nombre' => 'Antonio','apellidos' => 'Manuel García','edad' => '33'),
+  array('id' => '101','nombre' => 'Julio','apellidos' => 'Álvarez López','edad' => '27'),
+  array('id' => '102','nombre' => 'Joel','apellidos' => 'Lázaro Gómez','edad' => '19')
+);
